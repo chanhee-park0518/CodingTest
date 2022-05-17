f9dd0e2464bc1ba6107dc16fbd2e0cf009280ad1
chanhee.park <84426836+chanhee-park0518@users.noreply.github.com>
2022-05-18 00:00:09 +0900

    Create 6603_로또.cpp

---
+++ code_plus/531_brute_force/6603_로또.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX_NUMBER 100
+
+int n, k;
+
+vector<int> choice(MAX_NUMBER);
+vector<int> arr(MAX_NUMBER);
+vector<int> input(MAX_NUMBER);
+
+void comp(int number, int idx){
+    if (idx > k){
+       for (int i = 1; i <= k; i++){
+           int index = choice[i];
+           cout<<input[index]<<' ';
+       }
+       cout<<'\n';
+       return;
+    }
+
+    if (number > n) {
+        return;
+    }
+
+    choice[idx] = arr[number];
+
+    comp(number + 1, idx + 1);
+    comp(number + 1, idx);
+}
+int main(){
+    while(1) {
+        cin>>n;
+        if (n == 0)
+            break;
+
+        fill(choice.begin(), choice.end(), MAX_NUMBER);
+        fill(arr.begin(), arr.end(), MAX_NUMBER);
+        fill(input.begin(), input.end(), MAX_NUMBER);
+
+        for (int i = 1; i <= n; i++)
+            cin>>input[i];
+
+        k = 6;
+
+        for (int i = 1; i <= n; i++){
+            arr[i] = i;
+        }
+
+        comp(1, 1);
+        cout<<'\n';
+    }
+
+    return 0;
+}
