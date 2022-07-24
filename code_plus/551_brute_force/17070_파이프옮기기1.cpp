6c4a383f7d2cf87ba7c8d6dc230445ff8f81f655
chanhee.park <84426836+chanhee-park0518@users.noreply.github.com>
2022-07-25 07:20:31 +0900

    Create 17070_파이프옮기기1.cpp

---
+++ code_plus/551_brute_force/17070_파이프옮기기1.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int arr[100][100];
+
+int func(int x, int y, int dir) {
+    if (x == n-1 && y == n-1) {
+        return 1;
+    }
+    int ans = 0;
+    if (dir == 0) {
+        if (y+1 < n && arr[x][y+1] == 0) {
+            ans += func(x, y+1, 0);
+        }
+        if (x+1 < n && y+1 < n && arr[x][y+1] == 0 && arr[x+1][y] == 0 && arr[x+1][y+1] == 0) {
+            ans += func(x+1, y+1, 1);
+        }
+    } else if (dir == 1) {
+        if (y+1 < n && arr[x][y+1] == 0) {
+            ans += func(x, y+1, 0);
+        }
+        if (x+1 < n && arr[x+1][y] == 0) {
+            ans += func(x+1, y, 2);
+        }
+        if (x+1 < n && y+1 < n && arr[x][y+1] == 0 && arr[x+1][y] == 0 && arr[x+1][y+1] == 0) {
+            ans += func(x+1, y+1, 1);
+        }
+    } else if (dir == 2) {
+        if (x+1 < n && arr[x+1][y] == 0) {
+            ans += func(x+1, y, 2);
+        }
+        if (x+1 < n && y+1 < n && arr[x][y+1] == 0 && arr[x+1][y] == 0 && arr[x+1][y+1] == 0) {
+            ans += func(x+1, y+1, 1);
+        }
+    }
+    return ans;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i<n; i++) {
+        for (int j = 0; j<n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    int result = func(0, 1, 0);
+
+    cout<<result<<'\n';
+
+    return 0;
+}
